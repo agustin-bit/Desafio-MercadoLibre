1380c201f24ae5b60638424bcd7eec178345caf1
mtsluna <mtsluna@gmail.com>
2021-01-21 01:16:02 -0300

    Algorithm in main

---
+++ src/main/java/meli/challenge/demo/MeliChallengeApplication.java
@@ -0,0 +1,206 @@
+package meli.challenge.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+@SpringBootApplication
+public class MeliChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliChallengeApplication.class, args);
+
+//		String[] dna = {
+//			"CAGTGCCCCCTA",
+//			"TTATGTCCCCTA",
+//			"AGAAGGCCCCTA",
+//			"CCCCCCCCAAAA",
+//			"TCACTGCCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//			"ATGCGACCCCTA",
+//		};
+
+//		String[] dna = {
+//					"ATGCGA",
+//					"CAGTGC",
+//					"TTATGT",
+//					"AGAAGG",
+//					"CCCCCA",
+//					"TCACTG"
+//				};
+
+		String[] dna = {
+				"ATGCGA",
+				"CAGTGC",
+				"TTATGT",
+				"AGAAGG",
+				"CCCCCA",
+				"TCACTG"
+		};
+
+		char[][] array2DDna = stringArrayTo2DCharArray(dna);
+		print(array2DDna);
+
+		//System.out.println("Horizontal -----");
+
+		//printColumn(array2DDna[4]);
+		//System.out.println(search(array2DDna[4]));
+
+		//System.out.println("Vertical -----");
+
+		//printColumn(getMatrixColumn(array2DDna, 4));
+		//System.out.println(search(getMatrixColumn(array2DDna, 4)));
+
+//		System.out.println("Oblicuo -----");
+//		System.out.println(search(getMatrixOblique(array2DDna, 0, 0)));
+
+//		System.out.println("Oblicuo inversa -----");
+//		System.out.println(search(getMatrixInverseOblique(array2DDna, 4, 5)));
+
+		resolve(array2DDna);
+
+	}
+
+	public static char[][] stringArrayTo2DCharArray(String [] dna){
+		char[][] matrix = new char[dna.length][dna.length];
+		for (int i = 0; i < dna.length; i++) {
+			for (int j = 0; j < dna.length; j++) {
+				matrix[i][j] = dna[i].charAt(j);
+			}
+		}
+		return matrix;
+	}
+
+	public static void print(char[][] dna){
+		for (char[] chars : dna) {
+			for (int j = 0; j < dna.length; j++) {
+				if (j == dna.length - 1)
+					System.out.print(chars[j]+"\n");
+				else{
+					System.out.print(chars[j]);
+				}
+			}
+		}
+	}
+
+	public static void printColumn(char [] row){
+		for (char c : row) {
+			System.out.println(c);
+		}
+	}
+
+	public static int search(char[] row){
+		char flag = ' ';
+		int letterCount = 0;
+		int sequenceCount = 0;
+		boolean restart = false;
+		for (char c : row) {
+			if (restart) {
+				flag = c;
+				restart = false;
+			}
+			if (flag == c) {
+				letterCount++;
+				if (letterCount == 4) {
+					sequenceCount++;
+					letterCount = 1;
+					restart = true;
+				}
+			} else {
+				letterCount = 1;
+				flag = c;
+			}
+		}
+		return sequenceCount;
+	}
+
+	public static char[] getMatrixColumn(char [][] matrix, int column){
+		char [] row = new char [matrix[1].length];
+		for (int i = 0; i < row.length; i++) {
+			row[i] = matrix[i][column];
+		}
+		return row;
+	}
+
+	public static char[] getMatrixOblique(char[][] matrix, int row, int column){
+		char [] result = new char[matrix.length];
+		int flag = 0;
+		while (row < matrix[0].length && column < matrix[1].length){
+			result[flag] = matrix[row][column];
+			flag++;
+			row++;
+			column++;
+		}
+		return result;
+	}
+
+	public static char[] getMatrixInverseOblique(char[][] matrix, int row, int column){
+		char[] result = new char[matrix.length];
+		int flag = 0;
+		while(row < matrix[0].length && column >= 0){
+			result[flag] = matrix[row][column];
+			flag++;
+			row++;
+			column--;
+		}
+		return result;
+	}
+
+	public static void resolve(char[][] matrix){
+		int row = matrix.length-1;
+		int column = 0;
+		while(column < matrix.length && row >= 0){
+
+			//In this line start the execution of the algorithm
+
+			System.out.println(matrix[row][column]);
+
+			if(column == 0){
+				System.out.println(search(matrix[row]));
+				if(row <= matrix.length - 4){
+					System.out.println(search(getMatrixOblique(matrix, row, column)));
+				}
+			}
+
+			if(row == 0 && column != 0 && column != matrix.length-1){
+				System.out.println(search(getMatrixColumn(matrix, column)));
+				if(column <= matrix.length - 4){
+					System.out.println(search(getMatrixOblique(matrix, row, column)));
+				}
+				if(column > matrix.length - 4){
+					System.out.println(search(getMatrixOblique(matrix, row, column)));
+				}
+			}
+
+			if(column == matrix.length-1 && row <= matrix.length - 4){
+				System.out.println(search(getMatrixInverseOblique(matrix, row, column)));
+			}
+
+			//End execution
+
+			if(column == matrix.length-1){
+				if(row < matrix.length-1){
+					row++;
+				}
+				else{
+					break;
+				}
+			}
+			if(row == 0 && column != matrix.length-1){
+				column++;
+			}
+			if(column == 0){
+				row--;
+			}
+		}
+	}
+
+}
---
+++ src/test/java/meli/challenge/demo/MeliChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.challenge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
